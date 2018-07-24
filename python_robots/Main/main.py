a78da8037fe353fb040a5b42cbfe1627c13b49ec
wby <1418885488@qq.com>
2018-07-25 00:39:53 +0800

    finshLogin

--- python_robots/Main/main.py
+++ python_robots/Main/main.py
@@ -1,5 +1,6 @@
 import requests
 import time
+import os
 
 BASE_URL = 'https://login.weixin.qq.com'
 class Main:
@@ -15,28 +16,49 @@ class Main:
         'lang': 'zh_CN',
         '_': int(time.time()),
         }
-        r = requests.get(url=url, params=params)
+        # print(url)
+        r = self.session.get(url=url, params=params)
         data = r.text.split(';')
         code = data[0].split('=')[1]
-        uuid = data[1].split('=',1)[1][2:14]
-        print(code)
-        print(uuid)
-        return uuid
+        # print(code)
+        if code == ' 200' :
+            self.uuid =  data[1].split('=',1)[1][2:14]
+        else:
+            raise Exception('getUUID Failed')
 
     def getQrcode(self):
-        uuid = getUUID()
-        url = 'https://login.weixin.qq.com/qrcode/' + uuid
-        r = requests.get(url=url)
+        url = BASE_URL + '/qrcode/' + self.uuid
+        r = self.session.get(url=url)
         with open('Qrcode.jpg', 'wb') as f:
             f.write(r.content)
-        print(url)
+        os.startfile('Qrcode.jpg')
 
-    def isLogin(self,uuid):
-        url = 'https://login.wx.qq.com/cgi-bin/mmwebwx-bin/login'
+    def isLogin(self):
+        url = BASE_URL + '/cgi-bin/mmwebwx-bin/login'
         params = {
             '_': int(time.time()),
             'loginicon' : 'true',
-            'r': '891911636',
+            # 'r': '891911636',
             'tip': 0,
-            'uuid':uuid[:12]
+            'uuid': self.uuid[:12]
         }
+        r = self.session.get(url=url, params=params)
+        data = r.text.split('=',2)
+        code = data[1][:3]
+        if code == '200':
+            url = data[2][1:-2] + '&fun=new'
+            r = self.session.get(url)
+            with open('get.txt', 'wb') as f:
+                f.write(r.content)
+            print(url)
+            return False
+        return True
+
+if __name__ == '__main__':
+    g = Main()
+    g.getUUID()
+    g.getQrcode()
+    g.isLogin()
+    while g.isLogin(): 
+        time.sleep(1)
+    print ('Done')
